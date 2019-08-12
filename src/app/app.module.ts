3b10af8de65e0fab9bf054ebe77994e90788f162
akhilesh.sharma <akhilesh.sharma@coriolis.co.in>
2019-08-12 19:10:46 +0530

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainUIComponent } from './main-ui/main-ui.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainUIComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main-ui/main-ui.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-ui',
+  templateUrl: './main-ui.component.html',
+  styleUrls: ['./main-ui.component.css']
+})
+export class MainUIComponent {
+}
